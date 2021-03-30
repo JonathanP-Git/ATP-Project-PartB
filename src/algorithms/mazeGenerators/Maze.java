ba0efc38913c5bdcc70e41c586f47d91ecb9765b
TalyaSakov <talyasa@post.bgu.ac.il>
2021-03-30 15:57:08 +0300

    keep merge

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -8,24 +8,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Maze {
-<<<<<<< HEAD
-    int [][] maze;
-    int rows_mazeSize;
-    int column_mazeSize;
-=======
     int[][] maze;
     int rows;
     int column;
->>>>>>> 35121aaf01746b6771859394f06a6fcfc93ac4b4
     Position end_position;
     Position start_position;
 
     public int getRows() {
-        return rows_mazeSize;
+        return rows;
     }
 
     public int getColumn() {
-        return column_mazeSize;
+        return column;
     }
 
     public int[][] getMaze() {
@@ -40,10 +34,6 @@ public class Maze {
 //            sb.append("\n");
 //        }
 //        return sb.toString();}
-<<<<<<< HEAD
-    public String toString(){
-
-=======
 
     public String toString(List<MazeState> list) {
         final char PASSAGE_CHAR = '*';
@@ -78,25 +68,16 @@ public class Maze {
     }
 
     public String toString() {
->>>>>>> 35121aaf01746b6771859394f06a6fcfc93ac4b4
         final char PASSAGE_CHAR = '*';
         final char WALL_CHAR = 'X';
-
         final StringBuffer b = new StringBuffer();
 //        for ( int x = 0; x < column + 2; x++ )
 //            b.append( WALL_CHAR );
 //        b.append( '\n' );
-<<<<<<< HEAD
-        for ( int x = 0; x < rows_mazeSize; x++ ){
-//            b.append( WALL_CHAR );
-            for ( int y = 0; y < column_mazeSize; y++ )
-                b.append( maze[x][y] == 1 ? WALL_CHAR : PASSAGE_CHAR );
-=======
         for (int x = 0; x < rows; x++) {
 //            b.append( WALL_CHAR );
             for (int y = 0; y < column; y++)
                 b.append(maze[x][y] == 1 ? WALL_CHAR : PASSAGE_CHAR);
->>>>>>> 35121aaf01746b6771859394f06a6fcfc93ac4b4
 //            b.append( WALL_CHAR );
             b.append('\n');
         }
@@ -108,16 +89,6 @@ public class Maze {
 
 
     public Maze(int rows, int column) {
-<<<<<<< HEAD
-        this.rows_mazeSize = rows;
-        this.column_mazeSize = column;
-        this.end_position = new Position(0,0);
-        maze = new int[rows][column];
-        int i,j;
-        for  (i=0;i<this.column_mazeSize;i++){
-            for (j=0;j<this.rows_mazeSize;j++){
-                maze[j][i]=1;
-=======
         this.rows = rows;
         this.column = column;
         this.end_position = new Position(0, 0);
@@ -126,7 +97,6 @@ public class Maze {
         for (i = 0; i < this.column; i++) {
             for (j = 0; j < this.rows; j++) {
                 maze[j][i] = 1;
->>>>>>> 35121aaf01746b6771859394f06a6fcfc93ac4b4
             }
         }
     }
